929b5aa9108e5b8d816b2138aaaaa4276e5ee41a
Yuma Ishikawa <yuma.fuu05@gmail.com>
2023-07-30 19:49:18 +0900

    [fix] puppeteer

--- puppeteer-deno/index.ts
+++ puppeteer-deno/index.ts
@@ -1,4 +1,4 @@
-import { default as puppeteer } from 'npm:puppeteer';
+import puppeteer from "https://deno.land/x/puppeteer@16.2.0/mod.ts";
 
 const browser = await puppeteer.launch({ headless: "new" });
 const page = await browser.newPage();
